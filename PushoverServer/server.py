8fb2e94b266ff3fdeebe4eedd2bb63326137dbe6
Lanjelin <lanjelin.svanes@gmail.com>
2022-01-02 14:47:38 +0100

    Update server.py

--- PushoverServer/server.py
+++ PushoverServer/server.py
@@ -6,26 +6,31 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return make_response('POST a request to /push with at least token, user and message as data.', 200, None)
+    return make_response(
+        "POST a request to /push with at least token, user and message as data.",
+        200,
+        None,
+    )
 
 
-@app.route('/push', methods=['POST'])
+@app.route("/push", methods=["POST"])
 def push():
     content = request.get_json()
-    if ('user' not in content) or ('token' not in content) or ('message' not in content):
+    if not all(x in content for x in ["user", "token", "message"]):
         os.system(f"echo Missing input data: {content} > /proc/1/fd/1")
-        return make_response("Missing input data", 422)
+        return make_response("Missing either user, token or message in data.", 422)
     else:
         push = Pushover()
         result = push.message(**content)
-        if result.answer['status'] == 1:
+        if result.answer["status"] == 1:
             os.system(f"echo Done: {result.answer} > /proc/1/fd/1")
-            return make_response("Done", 200)
+            return make_response(f"{result.answer}", 200)
         else:
             os.system(f"echo Bad Request: {result.answer} > /proc/1/fd/1")
-            return make_response("Bad Request", 400)
+            return make_response(f"{result.answer}", 400)
 
 
-app.run(host='0.0.0.0', port=5000)
+app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
